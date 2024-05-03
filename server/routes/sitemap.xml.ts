ef4b03979e02e19e192116b43a0c83c0fddd851a
Efu <o@efu.me>
2024-05-03 09:49:15 +0800

    change: sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
-    hostname: 'https://docs.efu.me',
+    hostname: 'https://solitude.js.org',
   })
 
   for (const doc of docs) {
